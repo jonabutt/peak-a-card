d031b6c7b3b989f0e0c0a32f37bc54843f411ad1
jonabutt <jonathanbuttigieg1@gmail.com>
2022-01-03 21:33:18 +0100

    create gameboard scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,25 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Peak a Card 3</h1>
+      <h1>Peak a Card 3 d</h1>
+      <section className="game-board">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </section>
     </div>
   );
 }
