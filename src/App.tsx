8d42800bfafe50a53ded6ff736e93ce49207fc81
jonabutt <jonathanbuttigieg1@gmail.com>
2022-01-04 21:22:56 +0100

    feat: card have front and back and can be flipped

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { Card } from './Components/Card';
 
 const App: React.FC = () => {
-  const [cardList, setCardList] = useState<number[]>([])
+  const [cardList, setCardList] = useState<{value: number,isShowingFront: boolean,index: number}[]>([])
   const [firstLoad, setFirstLoad] = useState<boolean>(true)
   useEffect(() => {
       if(firstLoad){
         setFirstLoad(false);
         for(let i =0;i<16;i++){
-          setCardList(cardList => [...cardList,i]);
+          setCardList(cardList => [...cardList,{value:i, isShowingFront: false, index:i}]);
         }
       }
       
   },[firstLoad])
+  const handleClickCard = (index: number) => {
+    let tempCardList = [...cardList];
+    tempCardList[index].isShowingFront = true;
+    setCardList(tempCardList);
+  }
   return (
     <div>
       <h1>Peak a Card 3  s d</h1>
       <section className="game-board">
        
         {
-          cardList.map((c,index)=><Card key={index} value={c}/>)
+          cardList.map(c=>
+              <Card
+                key={c.index} 
+                value={c.value}
+                index={c.index}
+                isShowingFront={c.isShowingFront}
+                handleClickCard={handleClickCard}
+              />
+          )
         }
       </section>
     </div>
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 interface Props {
-    value: number
+    index: number,
+    value: number,
+    isShowingFront: boolean,
+    handleClickCard: (value: number) => void
+
 }
 
-export const Card: React.FC<Props> = ({value}) => {
-        return (
-            <div className="card">
-                {value}
-            </div>
-        );
+const CardStyle = styled.div`
+    
+    .card-face{
+        width: 100%;
+        height: 100%;
+        border-radius: 4px;
+        box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.57);
+        cursor: pointer;
+      
+        &.card-front{
+            background: red;
+        }
+        &.card-back{
+            background: rgb(83, 19, 213);
+            background: linear-gradient(
+              321deg,
+              rgba(83, 19, 213, 1) 25%,
+              rgba(108, 22, 157, 1) 75%
+            );
+            
+        }
+    }
+`
+
+export const Card: React.FC<Props> = ({index,value,isShowingFront,handleClickCard}) => {
+    return (
+        <CardStyle onClick={()=>handleClickCard(index)}>
+            {isShowingFront &&
+                <div className="card-face card-front">
+                    {value}
+                </div>
+            }
+            {!isShowingFront &&
+                <div className="card-face card-back">
+
+                </div>
+            }
+        </CardStyle>
+    );
 }
