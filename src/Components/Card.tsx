92b8fb1b98731bfcaef5c66ba6e4c7e991e4ad2c
jonabutt <jonathanbuttigieg1@gmail.com>
2022-01-07 22:02:53 +0100

    feat: added flip animation on card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -10,7 +10,12 @@ interface Props {
 }
 
 const CardStyle = styled.div`
-    
+  
+    transition: 1s transform ease-in;
+    transform-style: preserve-3d;
+    &.is-flipped{
+        transform: rotateY(180deg);
+    }
     .card-face{
         width: 100%;
         height: 100%;
@@ -21,9 +26,13 @@ const CardStyle = styled.div`
         display: flex;
         justify-content: center;
         align-items: center;
+        backface-visibility: hidden;
+        position: absolute;
         &.card-front{
             background: rgb(22,157,123);
-            background: linear-gradient(321deg, rgba(22,157,123,1) 25%, rgba(54,185,226,1) 75%);
+            background: linear-gradient(321deg, rgba(22,157,123,1) 25%, rgba(54,185,226,1) 75%);     
+            
+            transform: rotateY(180deg);
         }
         &.card-back{
             background: rgb(83, 19, 213);
@@ -32,7 +41,6 @@ const CardStyle = styled.div`
               rgba(83, 19, 213, 1) 25%,
               rgba(108, 22, 157, 1) 75%
             );
-            
         }
         .icon-checkmark{
             position: absolute;
@@ -44,18 +52,14 @@ const CardStyle = styled.div`
 
 export const Card: React.FC<Props> = ({index,value,isShowingFront,matched,handleClickCard}) => {
     return (
-        <CardStyle onClick={()=>handleClickCard(index,value)}>
-            {isShowingFront &&
-                <div className="card-face card-front">
-                    <img src={`/images/${value}.png`} alt="checkmark" />
-                    {matched && <img src="/images/checkmark.svg" alt="checkmark" className='icon-checkmark'/>}
-                </div>
-            }
-            {!isShowingFront &&
-                <div className="card-face card-back">
+        <CardStyle onClick={()=>handleClickCard(index,value)} className={isShowingFront?"is-flipped":""}>
+            <div className={"card-face card-front"}>
+                <img src={`/images/${value}.png`} alt="checkmark" />
+                {matched && <img src="/images/checkmark.svg" alt="checkmark" className='icon-checkmark'/>}
+            </div>
+            <div className='card-face card-back'>
 
-                </div>
-            }
+            </div>
         </CardStyle>
     );
 }
